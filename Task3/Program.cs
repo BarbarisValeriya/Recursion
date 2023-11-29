c59f07dda7b31d2bba2b8ae231b77531f7aa3f4d
Valeriia <borisova.valeriya.n@mail.ru>
2023-11-29 16:21:46 +0300

    я пыталась без реверса, но все тщетно

---
+++ Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+Random rand = new Random();
+int length = 10;
+int[] array = new int[length];
+int A = array[0];
+int B = array[9];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-100, 100);
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+Console.WriteLine("обратный порядок массива:");
+
+// Я пыталась:(
+
+void PrintReverse (int number)
+{
+    if (number == array[0]) return;
+    PrintReverse (array[array.Length - 1]);
+    Console.Write(number + " ");
+}
+
+PrintReverse(B);
+
+/*Array.Reverse(array);
+Console.WriteLine(String.Join(',', array));*/
+
+
+
+
+
+
+
+
