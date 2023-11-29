4ef5baa167c8dc8659e665fc1026cf7d7c098348
Valeriia <borisova.valeriya.n@mail.ru>
2023-11-29 14:22:35 +0300

    задача 1 решена рекурсией

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.WriteLine("введите начальное число");
+int minimum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите конечное число");
+int maximum = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers (int number)
+{
+    if (number < minimum) return;
+    PrintNumbers (number-1);
+    Console.Write(number + " ");
+}
+
+PrintNumbers(maximum);
